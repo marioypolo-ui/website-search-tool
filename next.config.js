5b001d5aff3da3e1fcf75ba2b856f5a315cb4b6b
marioypolo-ui <mario.y.polo@gmail.com>
2025-08-05 10:04:59 +0800

    Remove deprecated appDir config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  // 移除了 experimental.appDir，因为在 Next.js 14.0.3 中 appDir 已经是默认启用的
 }
 
 module.exports = nextConfig
